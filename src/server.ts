8b98b518b372744f009db74a9a71fc3476690b60
Ketan Mehta <meetketanmehta@gmail.com>
2023-01-05 02:53:04 +0530

    creating custom middleware 2.18

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
+import cors from 'cors';
 import express from "express";
+import morgon from 'morgan';
+import router from "./router";
+
 const app = express();
 
+const customLogger = (message) => (req, res, next) => {
+	console.log("Custom logger");
+	next();
+}
+app.use(cors());
+app.use(morgon('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(customLogger('custom loooger'));
 app.get("/", (req, res) => {
 	console.log("Hello from express");
 	res.status(200).json({ message: "Hello" });
 });
+
+app.use('/api', router)
 
 export default app;
