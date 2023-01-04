01b7a9492d77dbdddd2c26272904d85b1f1d8ed0
Ketan Mehta <meetketanmehta@gmail.com>
2023-01-04 22:23:44 +0530

    migrations 1.33

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+	console.log("Hello from express");
+	res.status(200).json({ message: "Hello" });
+});
+
+export default app;
